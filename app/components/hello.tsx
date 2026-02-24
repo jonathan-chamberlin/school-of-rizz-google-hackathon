9e50b210d45d8862000a03ea80ac40fbb2826162
Jonathan Chamberlin <jcham17x@gmail.com>
2026-02-24 17:02:09 -0500

    Fix Hello component: capitalize name and import for JSX recognition

--- app/components/hello.tsx
+++ app/components/hello.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
-
-function hello() {
+'use client';
+const Hello = () => {
   return (
     <div>hello</div>
   )
 }
 
-export default hello
+export default Hello
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Hello from "@/app/components/hello"
 
 const Home = () => {
   return (
+    <main>
     <div className="text-5xl underline"> Welcome to School of Rizz</div>
+    <Hello />
+
+    </main>
   )
 }
 
